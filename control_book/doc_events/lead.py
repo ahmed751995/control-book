936569f48c167a1cfb5d6236cd5c4f4056c43751
Ahmed abdel Rahman <ahmed@42.guru>
2024-10-19 22:57:56 +0300

    update pic

--- control_book/doc_events/lead.py
+++ control_book/doc_events/lead.py
@@ -1,3 +1,4 @@
 def before_save(doc, method=None):
     for item in doc.custom_bill_of_quantity:
-        item.preview_image = f"<img src='{item.attach_image_wjpb}' width=32 style='margin-top: -6px; height: 32px !important; ' "
+        if item.attach_image_wjpb:
+            item.preview_image = f"<img src='{item.attach_image_wjpb}' width=32 style='margin-top: -6px; height: 32px !important; ' "
